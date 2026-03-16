136026df51644e8c476fef7c3961c48119451ffa
Александр Уколов <aggink@mail.ru>
2026-03-16 16:43:01 +0300

    [Feature] Добавлен шаблон для Л/Р №1

---
+++ src/Laba1/Study.LabWork1/Program.cs
@@ -0,0 +1,39 @@
+using Study.LabWork1.Shared.Services;
+
+namespace Study.LabWork1;
+
+/// <summary>
+/// Начальная точка входа
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Номер выполняемой задачи
+    /// </summary>
+    private const int RUN_TASK_NUMBER = 1;
+
+    /// <summary>
+    /// Старт программы
+    /// </summary>
+    public static void Main()
+    {
+        var service = new RunService();
+
+        // todo: можно переписать на ввод с консоли
+
+        switch (RUN_TASK_NUMBER)
+        {
+            case 1:
+                service.RunTask1();
+                break;
+            case 2:
+                service.RunTask2();
+                break;
+            case 3:
+                service.RunTask3();
+                break;
+            default:
+                throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Laba1/Study.LabWork1/Readme.md
@@ -0,0 +1,37 @@
+<a name='assembly'></a>
+# Study.LabWork1
+
+## Contents
+
+- [Program](#T-Study-LabWork1-Program 'Study.LabWork1.Program')
+  - [RUN_TASK_NUMBER](#F-Study-LabWork1-Program-RUN_TASK_NUMBER 'Study.LabWork1.Program.RUN_TASK_NUMBER')
+  - [Main()](#M-Study-LabWork1-Program-Main 'Study.LabWork1.Program.Main')
+
+<a name='T-Study-LabWork1-Program'></a>
+## Program `type`
+
+##### Namespace
+
+Study.LabWork1
+
+##### Summary
+
+Начальная точка входа
+
+<a name='F-Study-LabWork1-Program-RUN_TASK_NUMBER'></a>
+### RUN_TASK_NUMBER `constants`
+
+##### Summary
+
+Номер выполняемой задачи
+
+<a name='M-Study-LabWork1-Program-Main'></a>
+### Main() `method`
+
+##### Summary
+
+Старт программы
+
+##### Parameters
+
+This method has no parameters.
